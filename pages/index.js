c05693b93e5ca87ba22cb651d9d7cb0e137a89c1
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-01-23 17:20:21 +0600

    nvigated component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>Hello horld</h1>
+      <Link href="/blog"><a>Blog</a></Link>
+      <Link href="/product"><a>products</a></Link>
     </div>
   )
 }
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,11 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
-const index = () => {
+const index = ({productId=100}) => {
     return (
         <div>
-            <h1>Product 1</h1>
-            <h1>Product 2</h1>
-            <h1>Product 3</h1>
+            <Link href="/"><a>HOme</a></Link>
+            <Link href={`/product/${productId}`}><a>product {productId}</a></Link>
+            <Link href="/product/2"><a>product2</a></Link>
+            <Link href="/product/3" replace><a>product3</a></Link>
         </div>
     );
 };
