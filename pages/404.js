871dd3e4d0ff09868e389a130028f0851d5fc165
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-01-24 11:06:33 +0600

    jcreated jfirst custom 404 page with nextjs

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notFOundPage = () => {
+    return (
+        <div>
+            <h1>This is our custom 404 page. We are not focusing on design right now.</h1>
+        </div>
+    );
+};
+
+export default notFOundPage;
