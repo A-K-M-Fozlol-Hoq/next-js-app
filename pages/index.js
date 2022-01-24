965c5cb5bfbefdd0dbb93fff234cb88effee20d5
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-01-24 11:00:54 +0600

    navigated programitacally

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Link from "next/link";
+import {useRouter} from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log('placing order successfully')
+    router.push('/product');
+    // router.replace('/product');
+  }
   return (
     <div>
       <h1>Hello horld</h1>
       <Link href="/blog"><a>Blog</a></Link>
       <Link href="/product"><a>products</a></Link>
+      <button onClick={handleClick}>
+        Place Order
+      </button>
     </div>
   )
 }
